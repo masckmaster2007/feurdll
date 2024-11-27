3ca764cca466dc27b2577175e1e9d53114d741c2
Jean <63162857+masckmaster2007@users.noreply.github.com>
2024-11-27 20:29:30 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -129,7 +129,6 @@ class $modify(MySecretLayer5, SecretLayer5) {
 		    if (web::WebResponse* res = e->getValue()) {
 			std::string tesla = res->string().unwrapOr("0");
 			if(tesla != "0") {
-				SecretLayer5::showSuccessAnimation();
 				web::openLinkInBrowser(tesla);
 				return;
 			}
@@ -142,7 +141,8 @@ class $modify(MySecretLayer5, SecretLayer5) {
 
 	        auto req = web::WebRequest();
 	        // Let's fetch... uhh...
-		std::string url = std::format("https://gdps.dimisaio.be/database/getTesla.php?key={}", text);
+		std::string url = "https://gdps.dimisaio.be/database/getTesla.php?key=";
+		url += text;
 	        m_fields->m_listener.setFilter(req.get(url));
 
 		SecretLayer5::onSubmit(sender);
